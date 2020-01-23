c67be300b5b9432fdc6edd589f244226b588a3c7
Mathieu Eveillard <mathieu.eveillard@gmail.com>
2020-01-23 11:51:57 +0100

    Leap Year Kata

---
+++ src/main/java/fr/arolla/leapyear/LeapYear.java
@@ -0,0 +1,20 @@
+package fr.arolla.leapyear;
+
+public class LeapYear {
+    public static boolean isMultipleOf(int modulo, int year) {
+        return year % modulo == 0;
+    }
+
+    public static boolean isLeapYear(int year) {
+        if(isMultipleOf(400, year)) {
+            return true;
+        }
+        if(isMultipleOf(100, year)) {
+            return false;
+        }
+        if(isMultipleOf(4, year)) {
+            return true;
+        }
+        return  false;
+    }
+}
